2a4448d6499a8175ecec9b237c76c8ec5a783e2c
SOFTSERVE\abilya <abilya@softserveinc.com>
2019-09-13 12:17:04 +0300

    SN: updates

--- sport-news-app/src/app/login/singin/singin.component.ts
+++ sport-news-app/src/app/login/singin/singin.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
-import { AmplifyService } from 'aws-amplify-angular'
-import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms'
-import { FlashMessagesService } from 'angular2-flash-messages'
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AmplifyService } from 'aws-amplify-angular';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Component({
   selector: 'app-singup',
   templateUrl: './singin.component.html',
   styleUrls: ['./singin.component.css'],
 })
 export class SinginComponent implements OnInit {
-  signInForm: FormGroup
-  submitted: boolean = false
-  authService: any
+  signInForm: FormGroup;
+  submitted: boolean = false;
+  authService: any;
 
   constructor(
     private amplifyService: AmplifyService,
@@ -22,21 +22,21 @@ export class SinginComponent implements OnInit {
 
   ngOnInit() {
     this.signInForm = this.formBuilder.group({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    })
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
   }
 
   get f() {
-    return this.signInForm.controls
+    return this.signInForm.controls;
   }
 
   logIn() {
-    this.submitted = true
-    const values = this.signInForm.value
+    this.submitted = true;
+    const values = this.signInForm.value;
 
     if (this.signInForm.invalid) {
-      return
+      return;
     }
 
     this.amplifyService
@@ -46,24 +46,24 @@ export class SinginComponent implements OnInit {
         password: values.password,
       })
       .then(() => {
-        this.router.navigate(['./home'])
+        this.router.navigate(['./home']);
       })
       .catch(err => {
         // TODO: replace with FormControl methods
-        const elements = document.querySelectorAll('.form-control')
+        const elements = document.querySelectorAll('.form-control');
         for (let i = 0; i < elements.length; i++) {
-          elements[i].classList.remove('valid-input')
-          elements[i].classList.add('invalid-input')
+          elements[i].classList.remove('valid-input');
+          elements[i].classList.add('invalid-input');
         }
 
         if (err.code === 'NotAuthorizedException' || 'UserNotFoundException') {
           this.flashMessagesService.show(`Incorrect user ID or password.Try again`, {
             cssClass: 'alert-danger',
             timeout: 5000,
-          })
+          });
         } else {
-          this.flashMessagesService.show(err.message, { cssClass: 'alert-danger', timeout: 5000 })
+          this.flashMessagesService.show(err.message, { cssClass: 'alert-danger', timeout: 5000 });
         }
-      })
+      });
   }
 }
