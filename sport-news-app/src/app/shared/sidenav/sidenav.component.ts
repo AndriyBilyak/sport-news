0f9f4a8997c6fd135bbcf3ad23a5dcbcf2bc1785
Ivan Dubravskyi <ivan.dybravskyi@gmail.com>
2019-09-16 22:56:50 +0300

    featere/SN-41 left menu

--- sport-news-app/src/app/shared/sidenav/sidenav.component.ts
+++ sport-news-app/src/app/shared/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router,} from '@angular/router';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import { Router, } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,15 +7,44 @@ import { Router,} from '@angular/router';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+  @ViewChild('toggleInput', {static: false}) toggleInput: HTMLInputElement;
   navigationItems = [];
+  opened = window.innerWidth > 768;
+  mobile = window.innerWidth < 768;
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.opened = window.innerWidth > 768;
+    this.mobile = window.innerWidth > 768;
+  }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
-    this.getNavigationItems()
+    this.getNavigationItems();
+    console.log(this.navigationItems);
   }
 
   private getNavigationItems() {
-    this.navigationItems = this.router.config.map(routes => routes.path)
+    this.navigationItems = this.router.config.map(routes => {
+      return {
+        path: routes.path,
+        active: (routes.path === 'home')
+      };
+    });
+  }
+
+  changeActive(menuItem) {
+    if (window.innerWidth < 768) {
+      this.opened = false;
+    }
+    this.navigationItems.forEach((item) => {
+      item.active = false;
+    });
+    menuItem.active = true;
+  }
+
+  toggleSideNav() {
+    this.opened = !this.opened;
   }
 }
