69ccacff11c92091fb78d7ea9284329a6b31a2e4
kitmurkit2 <36011861+kitmurkit2@users.noreply.github.com>
2019-09-16 20:36:35 +0300

    Sn-40/removed mostComments duplication and improved mostComments

--- sport-news-app/src/app/dealbook/dealbook.module.ts
+++ sport-news-app/src/app/dealbook/dealbook.module.ts
@@ -5,12 +5,11 @@ import { DealbookRoutingModule } from './dealbook-routing.module';
 import { DealbookComponent } from './dealbook.component';
 import { MainArticleComponent } from './main-article/main-article.component';
 import { ArticleComponent } from './article/article.component';
-import { MostPopularCommentsComponent } from './most-popular-comments/most-popular-comments.component';
 import { AdBlockComponent } from './ad-block/ad-block.component';
 
 
 @NgModule({
-  declarations: [DealbookComponent, MainArticleComponent, ArticleComponent, MostPopularCommentsComponent, AdBlockComponent],
+  declarations: [DealbookComponent, MainArticleComponent, ArticleComponent, AdBlockComponent],
   imports: [
     CommonModule,
     DealbookRoutingModule
