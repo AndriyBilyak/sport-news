9984632e8b4c45a937f6121a0e7da158945d94bf
kitmurkit2 <36011861+kitmurkit2@users.noreply.github.com>
2019-09-21 16:39:18 +0300

    SN-42/ added team-hub

--- sport-news-app/src/app/profile/profile.module.ts
+++ sport-news-app/src/app/profile/profile.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
@@ -8,12 +10,14 @@ import { ChangePasswordComponent } from './change-password/change-password.compo
 import { SurveysComponent } from './surveys/surveys.component';
 import { TeamHubComponent } from './team-hub/team-hub.component';
 
-
 @NgModule({
-  declarations: [ProfileComponent, PersonalComponent, ChangePasswordComponent, SurveysComponent, TeamHubComponent],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+  declarations: [
+    ProfileComponent,
+    PersonalComponent,
+    ChangePasswordComponent,
+    SurveysComponent,
+    TeamHubComponent,
+  ],
+  imports: [CommonModule, ProfileRoutingModule, FormsModule, NgbModule],
 })
-export class ProfileModule { }
+export class ProfileModule {}
