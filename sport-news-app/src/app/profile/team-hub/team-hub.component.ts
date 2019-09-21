a7a1bf16e9e6e851ef1fab5059b60cd32b62c5e2
kitmurkit2 <36011861+kitmurkit2@users.noreply.github.com>
2019-09-21 16:54:32 +0300

    SN-42/added team-hub

--- sport-news-app/src/app/profile/team-hub/team-hub.component.ts
+++ sport-news-app/src/app/profile/team-hub/team-hub.component.ts
@@ -1,49 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { debounceTime, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-
-const allTeams: { name: string; photo: string; content: string }[] = [
-  {
-    photo: '../../../../assets/img/teams/barcelona.png',
-    name: 'FC Barcelona',
-    content: 'Some text',
-  },
-  {
-    photo: '../../../../assets/img/teams/atalanta.svg',
-    name: 'Atalanta B.C.',
-    content: 'Some text',
-  },
-  {
-    photo: '../../../../assets/img/teams/napoli.svg',
-    name: 'S.S.C. Napoli',
-    content: 'Some text',
-  },
-  {
-    photo: '../../../../assets/img/teams/internazionale.svg',
-    name: 'FC Internazionale Milano',
-    content: 'Some text',
-  },
-  {
-    photo: '../../../../assets/img/teams/milan.svg',
-    name: 'A.C. Milan',
-    content: 'Some text',
-  },
-  {
-    photo: '../../../../assets/img/teams/fiorentina.svg',
-    name: 'ACF Fiorentina',
-    content: 'Some text',
-  },
-  {
-    photo: '../../../../assets/img/teams/juventus.svg',
-    name: 'Juventus F.C.',
-    content: 'Some text',
-  },
-  {
-    photo: '../../../../assets/img/teams/roma.svg',
-    name: 'A.S. Roma',
-    content: 'Some text',
-  },
-];
+import allTeams from '../../mockedData/teamHub.js';
 
 let selectedTeams1 = [
   {
